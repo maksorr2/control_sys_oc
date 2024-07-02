3ca091463f84a6cbf8d1e601cf27a242ba53b7f8
maksorr2 <vanya.markov.75@bk.ru>
2024-07-02 12:56:55 +0300

    Update update.lua

--- update.lua
+++ update.lua
@@ -9,6 +9,8 @@ local download = {
 if fs.path('home/control_sys') ~= nil then
     fs.makeDirectory('home/control_sys')
     fs.open('home/control_sys')
+else
+    print('Файл уже существует')
 end
 --for i in download do
 --    shell.execute('wget https://github.com/maksorr2/control_sys_oc/'..download)
