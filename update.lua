3fa957dfef1a83a762b519d001e3ba56f4ce8410
maksorr2 <vanya.markov.75@bk.ru>
2024-07-02 12:20:52 +0300

    Update update.lua

--- update.lua
+++ update.lua
@@ -6,7 +6,10 @@ local download = {
     'test.lua',
     'utiles/modem.lua'
 }
-
-for i in download do
-    shell.execute('wget https://github.com/maksorr2/control_sys_oc/'..download)
+if fs.path('home/control_sys') ~= nil then
+    fs.makeDirectory('home/control_sys')
+    fs.open('home/control_sys')
 end
+--for i in download do
+--    shell.execute('wget https://github.com/maksorr2/control_sys_oc/'..download)
+--end
