657f9b4ba3fef37e61cfde014566190af400f559
maksorr2 <vanya.markov.75@bk.ru>
2024-07-02 17:15:33 +0300

    Update update.lua

--- update.lua
+++ update.lua
@@ -11,10 +11,26 @@ local download = {
 }
 if fs.path('home/control_sys') then
     fs.makeDirectory('home/control_sys')
-    shell.setWorkingDirectory("/home/control_sys/")
+    fs.makeDirectory('home/control_sys/utils')
+    fs.makeDirectory('home/control_sys/localization')
+
     for i = 1, #download do
-        shell.execute('wget https://raw.githubusercontent.com/maksorr2/control_sys_oc/main/'..download[i])
+        if i <= 2 then
+          print(i)
+          shell.setWorkingDirectory("/home/control_sys")
+          shell.execute('wget https://raw.githubusercontent.com/maksorr2/control_sys_oc/main/'..download[i])
+        elseif i == 3 then
+          print(i)
+          shell.setWorkingDirectory("/home/control_sys/utils")
+          shell.execute('wget https://raw.githubusercontent.com/maksorr2/control_sys_oc/main/'..download[i])
+        elseif i > 3 then
+          print(i)
+          shell.setWorkingDirectory("/home/control_sys/localization")
+          shell.execute('wget https://raw.githubusercontent.com/maksorr2/control_sys_oc/main/'..download[i])
+        end
     end
+    shell.setWorkingDirectory("/home/control_sys")
 else
     print('Project was been downloaded already! | Проект уже существует!')
+    shell.setWorkingDirectory("/home/control_sys")
 end
